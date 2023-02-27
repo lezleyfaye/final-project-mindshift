1bb7b87ef1a763a5a003e3bb2498801136c0f318
Lezley Faye <lezleydavidson@gmail.com>
2023-02-27 11:55:44 +0100

    Added site pages

---
+++ app/login/page.js
@@ -0,0 +1,6 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function LoginPage() {
+  return <main>symptoms</main>;
+}
---
+++ app/symptoms/page.js
@@ -0,0 +1,6 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function SymptomsPage() {
+  return <main>symptoms</main>;
+}
